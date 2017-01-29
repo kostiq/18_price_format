79c56caf28ea5663388ee062479768eda3b360c3
k.kuzmin <k.kuzmin@npo-echelon.ru>
2017-01-29 23:50:52 +0300

    rewrite some test

--- tests.py
+++ tests.py
@@ -6,6 +6,6 @@ class TestFormatPrice(unittest.TestCase):
     def test_format_price(self):
         self.assertEqual('3 245',str(format_price(3245.00000)))
         self.assertEqual('1 111', str(format_price('1111,0000')))
-        self.assertEqual(None, format_price('asd'))
+        self.assertEqual(0, format_price('asd'))
         self.assertEqual('1 325.01', str(format_price(1325.0112312)))
         
