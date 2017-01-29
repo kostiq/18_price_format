07556f08a87b73f08817aecd5367837971759b6f
k.kuzmin <k.kuzmin@npo-echelon.ru>
2017-01-29 18:02:57 +0300

    add tests and func for format

--- format_price.py
+++ format_price.py
@@ -1,5 +1,23 @@
+import argparse
+import re
+import locale
+
+
 def format_price(price):
-    pass
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+    price = str(price).replace(',', '.')
+    price = float(price)
+
+    price = locale.currency(price, grouping=True)
+    price = re.sub(r'\.00$', '', price)
+    price = price.replace('$', '')
+    price = price.replace(',', ' ')
+    return price
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--price', help="Input price", required=True,  type=float)
+    args = parser.parse_args()
+    print (format_price(args.price))
--- tests.py
+++ tests.py
@@ -1,1 +1,9 @@
-# TODO place your tests here
+import unittest
+from format_price import format_price
+
+class TestFormatPrice(unittest.TestCase):
+
+    def test_format_price(self):
+        self.assertEqual('3 245',str(format_price(3245.00000)))
+        self.assertEqual('1 325.01', str(format_price(1325.0112312)))
+        
